3e7c7a227d55f5df7c193a6560ea5200afc255a8
Asad Ikhlas <33983874+asadikhlas@users.noreply.github.com>
2019-03-10 21:33:15 +0500

    final commit

---
+++ src/components/artist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ArtistContainer from '../containers/artist_container';
+
+const Artist = (props) => {
+    return(
+        <div>
+            <ArtistContainer {...props}/>
+        </div>
+    )
+}
+
+export default Artist;
